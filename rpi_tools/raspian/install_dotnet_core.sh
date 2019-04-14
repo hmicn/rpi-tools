21b812f368bfa44c99119db7b6478e27783ee87e
SAM <michael@example.com>
2019-04-14 14:55:30 -0400

    Adding dotnet core installation

---
+++ rpi_tools/raspian/install_dotnet_core.sh
@@ -0,0 +1,3 @@
+apt-get install libc6 libcurl3 libgcc1 libgssapi-krb5-2 libicu52 liblttng-ust0 libssl1.0.0 libstdc++6 libunwind8 libuuid1 zlib1g
+# publish the app with dotnet publish -r linux-arm
+# deploy the binaries on RPI from the PC: bin/Debug/app/linux-arm/publish
